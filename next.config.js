d790a01185efaa5f63e829ded9fa005fa866782f
eshantarneja <eshantarneja@gmail.com>
2025-06-27 11:44:03 -0400

    Add root TSConfig and update Next.js config to fix module resolution

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
+const path = require('path');
+
 const nextConfig = {
-  distDir: 'pricing-dashboard/.next'
+  distDir: 'pricing-dashboard/.next',
+  transpilePackages: ['pricing-dashboard'],
+  webpack: (config, { isServer }) => {
+    // Add our custom path resolving for @/ aliases
+    config.resolve.alias['@'] = path.join(__dirname, 'pricing-dashboard');
+    return config;
+  },
 };
 
 module.exports = nextConfig;
